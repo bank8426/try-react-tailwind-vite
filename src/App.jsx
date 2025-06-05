ce9d00aaea7df360362f2b4cd6787aed2f540fc4
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-05 15:49:21 +0700

    Add Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("")
+
   return (
-    <h1 className='text-3xl font-bold underline'>App</h1>
+    <main>
+      <div className='pattern'></div>
+
+      <div className='wrapper'>
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+
+    </main>
+
   )
 }
 
